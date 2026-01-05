676da3aa1c5573862e04a7270114bf6bd87bc554
Dakota Feist <dakotafeist@pm.me>
2026-01-04 20:13:21 -0600

    Added support for multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::Path;
 use std::process::exit;
 use clap::Parser;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    file_path: String,
+    #[arg(short, long, required = false, num_args = 1..)]
+    file_paths: Vec<String>,
 }
 fn main() {
     let args = Args::parse();
-    let file_path = Path::new(&args.file_path);
-    let file = match File::open(file_path) {
-        Ok(file) => file,
-        Err(why) => {
-            println!("Error opening file: {}. {}", file_path.display(), why);
-            exit(2);
-        },
-    };
-
-    let reader = BufReader::new(file);
-    let mut line_count = 0;
-    for line_result in reader.lines() {
-        line_count += 1;
-        let line = match line_result {
-            Ok(line) => line,
-            Err(why) => panic!("couldn't read line: {}. {}", line_count, why),
+    for file_path in args.file_paths {
+         let file = match File::open(&file_path) {
+            Ok(file) => file,
+            Err(why) => {
+                println!("Error opening file: {}. {}", file_path, why);
+                exit(2);
+            },
         };
-        println!("{}", line);
-        if line_count == 10 {
-            break
+        let reader = BufReader::new(file);
+        let mut line_count = 0;
+        for line_result in reader.lines() {
+            line_count += 1;
+            let line = match line_result {
+                Ok(line) => line,
+                Err(why) => panic!("couldn't read line: {}. {}", line_count, why),
+            };
+            println!("{}", line);
+            if line_count == 10 {
+                break
+            }
         }
     }
 }
